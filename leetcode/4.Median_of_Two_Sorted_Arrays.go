739d35fedae71b980c77d57ba9d2d93863825204
RayGreen <green@RayGreendeMacBook-Pro.local>
2020-08-17 00:21:03 +0800

    add 53,62,63

--- leetcode/4.Median_of_Two_Sorted_Arrays.go
+++ leetcode/4.Median_of_Two_Sorted_Arrays.go
@@ -2,4 +2,5 @@ package leetcode
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
+	return 0.0
 }
---
+++ leetcode/63.Unique_Paths_2.go
@@ -0,0 +1,36 @@
+package leetcode
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	if obstacleGrid[0][0] == 1 {
+		return 0
+	}
+
+	obstacleGrid[0][0] = 1
+	n, m := len(obstacleGrid), len(obstacleGrid[0])
+	for i := 1; i < n; i++ {
+		if obstacleGrid[i][0] == 1 {
+			obstacleGrid[i][0] = 0
+			continue
+		}
+		obstacleGrid[i][0] = obstacleGrid[i-1][0]
+	}
+	for i := 1; i < m; i++ {
+		if obstacleGrid[0][i] == 1 {
+			obstacleGrid[0][i] = 0
+			continue
+		}
+		obstacleGrid[0][i] = obstacleGrid[0][i-1]
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if obstacleGrid[i][j] == 1 {
+				obstacleGrid[i][j] = 0
+				continue
+			}
+			obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+		}
+	}
+
+	return obstacleGrid[n-1][m-1]
+}

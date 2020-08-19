26c0868a3f80d9d6fa7cad092bde29aa5890e94e
Green <gl12138@live.com>
2020-08-19 18:26:50 +0800

    add better in 120

--- leetcode/120.Triangle.go
+++ leetcode/120.Triangle.go
@@ -1,12 +1,13 @@
 package leetcode
 
+//用辅助空间,一维数组 DP
 func minimumTotal(triangle [][]int) int {
 	n := len(triangle)
 	dp := make([]int, n, n)
 	dp[0] = triangle[0][0]
 	for i := 1; i < n; i++ {
-		left,right := dp[0] + triangle[i][0], dp[i-1] + triangle[i][i]
-		for j := 1; j <= i-1; j++ {
+		left, right := dp[0] + triangle[i][0], dp[i-1] + triangle[i][i]
+		for j := i-1; j >= 1; j-- {
 			if dp[j-1] < dp[j] {
 				dp[j] = dp[j-1] + triangle[i][j]
 			} else {
@@ -23,4 +24,19 @@ func minimumTotal(triangle [][]int) int {
 		}
 	}
 	return min
+}
+
+//不用辅助空间，直接从下层往上层推
+func minimumTotal1(triangle [][]int) int {
+	for i := len(triangle)-1; i >= 1; i-- {
+		for j := 0; j <= i-1; j++ {
+			if triangle[i][j] < triangle[i][j+1] {
+				triangle[i-1][j] += triangle[i][j]
+			} else {
+				triangle[i-1][j] += triangle[i][j+1]
+			}
+		}
+	}
+
+	return triangle[0][0]
 }

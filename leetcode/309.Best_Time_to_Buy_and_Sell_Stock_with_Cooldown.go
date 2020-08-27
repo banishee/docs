63cba7d86c0e318ef1ab0b810fef6eb2f5d59365
Green <gl12138@live.com>
2020-08-27 18:46:19 +0800

    unfinish 309

---
+++ leetcode/309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func maxProfit4(prices []int) int {
+	n := len(prices)
+	dp := make([]int, n+1, n+1)
+	for i := 2; i < n; i++ {
+		max := 0
+		for j := 1; j < i; j++ {
+
+		}
+	}
+
+	return 0
+}

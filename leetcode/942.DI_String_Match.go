2450aa544fdbb089d933727d048e381b3743c81c
Ray Green <gl12138@live.com>
2020-10-27 16:20:47 +0800

    Create 942.DI_String_Match.go

---
+++ leetcode/942.DI_String_Match.go
@@ -0,0 +1,16 @@
+func diStringMatch(S string) []int {
+    maxNum, minNum := len(S), 0
+    res := make([]int, len(S)+1)
+    for i := 0; i < len(S); i++ {
+        if S[i] == 'I' {
+            res[i] = minNum
+            minNum++
+        } else {
+            res[i] = maxNum
+            maxNum--
+        }
+    }
+    res[len(S)] =  minNum
+    
+    return res
+}
